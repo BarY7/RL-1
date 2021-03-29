344d45cc13464b5426c3d75bdbfe84813b0561ef
Bar <baryaacovi7@gmail.com>
2021-03-29 14:50:29 +0300

    update code to show accuracy

--- question1_graphs.py
+++ question1_graphs.py
@@ -5,9 +5,13 @@ import question1_3 as q13
 import os
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
-epochs_1_1 = q11.question1_1()
-epochs_1_2 = q12.question1_2()
-epochs_1_3 = q13.question1_3()
+epochs_1_1,accuracy_1_1 = q11.question1_1()
+epochs_1_2,accuracy_1_2 = q12.question1_2()
+epochs_1_3,accuracy_1_3 = q13.question1_3()
+print_accuracy = "Accuracy for graph {} = {}"
+print(print_accuracy = "Accuracy for graph {} = {}".format(1, accuracy_1_1))
+print(print_accuracy = "Accuracy for graph {} = {}".format(2, accuracy_1_2 ))
+print(print_accuracy = "Accuracy for graph {} = {}".format(3, accuracy_1_3))
 epochs_array = range(1,101)
 plt.plot(epochs_array,epochs_1_1,label="1")
 plt.plot(epochs_array,epochs_1_2,label="2 - Better params")
